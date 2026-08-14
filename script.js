2fd0ab34f6fb90a7397874b7a1115358a1f342af
Lunga Simelane <123343114+Lunga22@users.noreply.github.com>
2026-08-14 16:29:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -370,7 +370,7 @@ function updateCartUI() {
             }).join('');
         }
     }
-    if (totalPriceEl) totalPriceEl.innerText = `R${total.toFixed(2)}`;
+    if (totalPriceRl) totalPriceRl.innerText = `R${total.toFixed(2)}`;
 }
 
 /* --- CHECKOUT & PAYSTACK PAYMENT --- */
